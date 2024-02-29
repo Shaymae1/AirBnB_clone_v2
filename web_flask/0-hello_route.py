efe3321e0aab2d988d41f9fbc699922558efb503
Shaymae1 <133395313+Shaymae1@users.noreply.github.com>
2024-02-29 21:37:43 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,35 +1,17 @@
+
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" 0. Script to start a Flask web application """
-
-from flask import Flask
-
-
-=======
-"""Starts Flask web app
-Listening on 0.0.0.0:5000
-Route '/' displays "Hello HBNB!"
 """
-from flask import Flask
+start Flask application
+"""
 
->>>>>>> 5b18bfbd20053619b84f584f55bbdfeba6abbdb3
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-<<<<<<< HEAD
-def hello_world():
-    """ Returns some text. """
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-=======
-def hello_route():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
->>>>>>> 5b18bfbd20053619b84f584f55bbdfeba6abbdb3
+    app.run(host='0.0.0.0', port='5000')
