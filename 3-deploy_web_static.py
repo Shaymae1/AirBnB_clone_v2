fe18733a74261760d3622c5be2fda5affb16e6f6
Shaymae1 <chaymaeelkarcha@gmail.com>
2024-02-29 15:09:15 -0800

    k1

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,13 +3,13 @@
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
 
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/school -u ubuntu
 """
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.160.77.90', '10.25.190.21']
+env.hosts = ['18.206.208.251', '54.84.73.100']
 
 
 def do_pack():
